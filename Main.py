bdf401bd9693c24856f2aacdbc808a9d3964a8dd
Prajwal David <prajwaldavid05052006@gmail.com>
2025-12-27 23:49:47 +0530

    initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+import os
+
+import eel
+
+eel.init("www")
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', block=True)
+# d:/HEL/venHEL/Scripts/python.exe d:/HEL/Main.py
+
