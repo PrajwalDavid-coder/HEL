02ffed16d5e4cf3e7dd51ffca5995e0393e25456
PrajwalDavid-coder <prajwaldavid05052006@gmail.com>
2025-12-28 18:39:01 +0530

    Add files via upload

--- www/main.js
+++ www/main.js
@@ -9,6 +9,32 @@ $(document).ready(function () {
         out:{
             effect: "bounceOut",
         },
-    })
+    });
 
+
+
+// Siriwave setup
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 290,
+    style: 'ios9',
+    amplitude: 0.5,
+    speed: 0.3
+  });
+  siriWave.start();
+  });
+
+  // Animate siriwave 
+   $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
+        },
 });
