c4fd1530c8af2a3945f6274a812c471ce230d097
PrajwalDavid-coder <prajwaldavid05052006@gmail.com>
2026-01-01 21:17:31 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 import os
-
 import eel
 
+from Engine.features import *
+from Engine.command import *
+
+
 eel.init("www")
+
+playAssistantSound()
+
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
